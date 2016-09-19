c664f4f9149dc7baacdf1d694a1786e122ba1021
Artem Denysov <denysov.artem@gmail.com>
2016-09-20 01:25:39 +0300

    Add usage examples with grunt and gulp (#3)
    * add usage examples with grunt and gulp
    
    * Update README.md
    
    * update changelog

---
+++ recipes/gulp/gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var psiNgrok = require('../../index');
+var port = 8000;
+
+var connectServer = function() {
+  return connect.server({
+    root: 'public',
+    port: port
+  });
+};
+
+function handleError(err) {
+  console.log(err.toString());
+  process.exit(-1);
+}
+
+gulp.task('pagespeed', function () {
+  psiNgrok({
+    pages: ['index.html', 'slow.html'],
+    port: port,
+    onBeforeConnect: connectServer,
+    onError: handleError,
+    options: {
+      threshold: 80
+    }
+  });
+});
+
+gulp.task('default', ['pagespeed']);
